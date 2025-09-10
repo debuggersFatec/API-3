94b2d7777dcdb253f3236d3a28062c3744302b46
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-09-10 04:44:19 -0300

    ✨ feat: add Header component

---
+++ front-end/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import { AvatarUser } from "./AvatarUser";
+export const Header = () => {
+  return (
+    <Box>
+      <AvatarUser
+        name="Matheus Karnas"
+        imageUrl=""
+      />
+    </Box>
+  );
+};
