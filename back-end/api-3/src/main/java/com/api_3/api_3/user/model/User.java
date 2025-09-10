ff809b18e95f5bfe26001542dc6cd1a362cf6dd3
Lucas Guerra <lucasguerrawr2004@gmail.com>
2025-09-10 09:47:57 -0300

    :sparkles: feat(auth): implement user registration and login endpoints

--- back-end/api-3/src/main/java/com/api_3/api_3/user/model/User.java
+++ back-end/api-3/src/main/java/com/api_3/api_3/user/model/User.java
@@ -1,5 +1,33 @@
 package com.api_3.api_3.user.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.ArrayList; 
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "users")
+@Data
+@NoArgsConstructor
+
 public class User {
-    
+    @Id
+    private String uuid; 
+
+    private String name;
+    private String email;
+    private String password;
+    private String img;
+
+    private List<Object> equipes = new ArrayList<>();
+    private List<Object> tasks = new ArrayList<>();
+    private List<Object> lixeira = new ArrayList<>();
+
+    public User(String name, String email , String password , String img){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.img = img;
+    }
 }
