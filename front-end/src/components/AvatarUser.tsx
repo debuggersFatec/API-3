7d2aa253ef715a6c0f853285fba02081f3bfa2ea
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-09-11 20:01:13 -0300

    feat(avatarUser): add size prop for customization

--- front-end/src/components/AvatarUser.tsx
+++ front-end/src/components/AvatarUser.tsx
@@ -1,15 +1,16 @@
-import { Avatar } from "@chakra-ui/react"
+import { Avatar } from "@chakra-ui/react";
 
 interface AvatarUserProps {
   name: string;
   imageUrl: string;
+  size?: "sm" | "md" | "lg" | "xl" | "2xl" | "full" | "2xs" | "xs";
 }
 
-export const AvatarUser = ({ name, imageUrl }: AvatarUserProps) => {
+export const AvatarUser = ({ name, imageUrl, size }: AvatarUserProps) => {
   return (
-    <Avatar.Root size={"md"} colorPalette="blue">
+    <Avatar.Root size={size || "md"} colorPalette="blue">
       <Avatar.Fallback name={name} />
       <Avatar.Image src={imageUrl} />
     </Avatar.Root>
-  )
-}
+  );
+};
