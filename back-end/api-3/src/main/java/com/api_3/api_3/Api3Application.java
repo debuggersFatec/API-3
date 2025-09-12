3ababa4e16ddf21033302897bcc533ef5e541658
KwMajor <majorhydra@protonmail.com>
2025-09-12 20:58:27 -0300

    :lock: Fix(security): Resolve JWT decoding and configuration errors

--- back-end/api-3/src/main/java/com/api_3/api_3/Api3Application.java
+++ back-end/api-3/src/main/java/com/api_3/api_3/Api3Application.java
@@ -1,8 +1,9 @@
 package com.api_3.api_3;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Api3Application {
