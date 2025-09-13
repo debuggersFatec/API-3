eb2c9667fe038991a186cbc17a4f0fb96087917f
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-09-13 15:37:06 -0300

    ✨ feat(tabs): add EquipeDashboard component

--- front-end/src/components/EquipeDashboard.tsx
+++ front-end/src/components/EquipeDashboard.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@chakra-ui/react";
 import { SectionHeader } from "./SectionHeader";
+import { EquipeTabs } from "./EquipeTabs";
 
 interface Equipe {
   uuid: string;
@@ -7,10 +8,10 @@ interface Equipe {
 }
 
 export const EquipeDashboard = ({ equipe }: { equipe: Equipe }) => {
-  console.log("EquipeDashboard renderizado com equipe:", equipe);
   return (
     <Box w={"100%"} display={"flex"} flexDir={"column"} alignItems={"center"}>
       <SectionHeader title={equipe.name} />
+      <EquipeTabs />
     </Box>
   );
 };
