d9f91b03563872411425c55af9e41b4626289d4a
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-09-24 10:02:39 -0300

    :zap: Feat(dashboard): add authentication check to redirect unauthenticated users to login

--- front-end/src/page/Dashboard.tsx
+++ front-end/src/page/Dashboard.tsx
@@ -1,7 +1,19 @@
 import { Header } from "@/components/Header";
 import { Sidebar } from "@/components/Sidebar";
+import { useAuth } from "@/context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+  const { user, token } = useAuth();
+
+  useEffect(() => {
+    if (user === null || token === null) {
+      navigate("/login");
+    }
+  }, [user, token, navigate]);
+
   return (
     <>
       <Header />
