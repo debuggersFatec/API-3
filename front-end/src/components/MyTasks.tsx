8a17d36ecabcb8335b5fff8832596c948e6d1715
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-09-26 21:49:01 -0300

    ✨ feat(MyTasks): pass user tasks to CheckList component

--- front-end/src/components/MyTasks.tsx
+++ front-end/src/components/MyTasks.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import { SectionHeader } from "./SectionHeader";
 import { CheckList } from "./CheckList";
+import { useAuth } from "@/context/useAuth";
 
 export const MyTasks = () => {
+  const { user } = useAuth();
   return (
     <Box w={"100%"} display={"flex"} flexDir={"column"} alignItems={"center"}>
       <SectionHeader title="Minhas tarefas" />
-      <CheckList />
+      <CheckList tasks={user?.tasks} />
     </Box>
   );
 };
