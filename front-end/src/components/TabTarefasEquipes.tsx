f654d108be07ebbf8b6d634de6dc4c03839869c0
Matheus Karnas Castillo <matheuskarnas1@gmail.com>
2025-09-26 21:52:13 -0300

    ✨ feat(tab-tarefas-equipes): pass team tasks as props to CheckList

--- front-end/src/components/TabTarefasEquipes.tsx
+++ front-end/src/components/TabTarefasEquipes.tsx
@@ -1,5 +1,10 @@
+import type { TaskTeam } from "@/types/task";
 import { CheckList } from "./CheckList";
 
-export const TabTarefasEquipes = () => {
-  return <CheckList />;
+interface TabTarefasEquipesProps {
+  tasks: TaskTeam[];
+}
+
+export const TabTarefasEquipes = ({ tasks }: TabTarefasEquipesProps) => {
+  return <CheckList tasks={tasks} />;
 };
